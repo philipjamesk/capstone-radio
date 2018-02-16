d2895792ea9d2707731b98e009db4ab2ceeecd30
Philip James <pjkelnhofer@gmail.com>
2018-02-17 04:07:44 +0900

    Added logos; deleted old test files

--- Station.py
+++ Station.py
@@ -1,5 +1,6 @@
 class Station:
     """docstring for Station."""
-    def __init__(self, name, address):
+    def __init__(self, name, address, logo):
         self.name = name
         self.address = address
+        self.logo = logo
--- radio.py
+++ radio.py
@@ -8,9 +8,11 @@ from Station import Station
 # pygame.init()
 
 # Make a list of stations
-station_list = [Station('KCRW Ecletic 24', 'http://media.kcrw.com/pls/kcrwmusic.pls'),
-                Station('triple j', 'http://www.abc.net.au/res/streaming/audio/mp3/triplej.pls'),
-                Station('WUMB', 'http://wumb.streamguys1.com/wumb919fast')]
+station_list = [Station('KCRW Ecletic 24', 'http://media.kcrw.com/pls/kcrwmusic.pls', 'img/logo/eclectic24_logo.png'),
+                Station('triple j', 'http://www.abc.net.au/res/streaming/audio/mp3/triplej.pls', 'img/logo/triplej_logo.png'),
+                Station('WUMB', 'http://www.wumb.org/listenlive/links/wumbfast.pls', 'img/logo/wumb_logo.png'),
+                Station('TSF Jazz', 'http://statslive.infomaniak.ch/playlist/tsfjazz/tsfjazz-high.mp3/playlist.pls', 'img/logo/tsfjazz_logo.png'),
+                Station('Double J', 'http://www.abc.net.au/res/streaming/audio/mp3/dig_music.pls', 'img/logo/doublej_logo.png')]
 
 
 # Print list of stations
