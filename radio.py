893bb8bde92426a3823e6afd0038363d6bd0383a
Philip James <pjkelnhofer@gmail.com>
2018-02-23 00:17:01 +0900

    Fixed playing_station

--- radio.py
+++ radio.py
@@ -14,17 +14,31 @@ from logo import Logo
 pygame.init()
 
 # Make a list of stations
-station_list = [Station('KCRW Ecletic 24', 'http://media.kcrw.com/pls/kcrwmusic.pls', 'img/logos/eclectic24_logo.png'),
-                Station('triple j', 'http://www.abc.net.au/res/streaming/audio/mp3/triplej.pls', 'img/logos/triplej_logo.png'),
-                Station('WUMB', 'http://www.wumb.org/listenlive/links/wumbfast.pls', 'img/logos/wumb_logo.png'),
-                Station('TSF Jazz', 'http://statslive.infomaniak.ch/playlist/tsfjazz/tsfjazz-high.mp3/playlist.pls', 'img/logos/tsfjazz_logo.png'),
-                Station('Double J', 'http://www.abc.net.au/res/streaming/audio/mp3/dig_music.pls', 'img/logos/doublej_logo.png')]
+station_list = [Station('KCRW Ecletic 24',
+                        'http://media.kcrw.com/pls/kcrwmusic.pls',
+                        'img/logos/eclectic24_logo.png'),
+                Station('triple j',
+                        'http://www.abc.net.au/res/streaming/audio/mp3/triplej.pls',
+                        'img/logos/triplej_logo.png'),
+                Station('WUMB',
+                        'http://www.wumb.org/listenlive/links/wumbfast.pls',
+                        'img/logos/wumb_logo.png'),
+                Station('TSF Jazz',
+                        'http://statslive.infomaniak.ch/playlist/tsfjazz/tsfjazz-high.mp3/playlist.pls',
+                        'img/logos/tsfjazz_logo.png'),
+                Station('Double J',
+                        'http://www.abc.net.au/res/streaming/audio/mp3/dig_music.pls',
+                        'img/logos/doublej_logo.png')]
 
 
 
 # Play a stream
-def playStation(stream):
-    playing_station = subprocess.Popen(["mplayer", "-playlist", stream, "-cache-min", "99"])
+def playStation(station):
+    playing_station = subprocess.Popen(["mplayer",
+                      "-playlist",
+                      station_list[station].address,
+                      "-cache-min",
+                      "99"])
     return playing_station
 
 # Close stream
@@ -40,6 +54,7 @@ def play():
     bg_color = (232, 222, 199)
 
     station = 2
+    playStation(station)
 
     # Start the main loop for the radio app
     while True:
