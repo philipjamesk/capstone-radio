79004fd7d33efb5d51e13f56d11c32645cb56a63
Philip James <pjkelnhofer@gmail.com>
2018-03-09 19:58:22 -0500

    Updated Logo Class

--- logo.py
+++ logo.py
@@ -2,18 +2,28 @@ import pygame
 
 class Logo(object):
     def __init__(self, screen, image):
+        #
+        #   To make a logs...
+        #   logo = Logo()
+        #
+
         """Create a logo for each radio station on the display."""
         self.screen = screen
 
         # Load the image and get make a rect for it
         self.image = pygame.image.load(image)
-        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.scale(self.image, (80, 80))
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
+        self.screen = screen
+        self.screen_rect = self.screen.get_rect()
 
-        # Put logo at center of screen_rect
-        self.rect.centerx = self.screen_rect.centerx
+        # Put logo at y center and off screen
+        self.rect.centerx = -40
         self.rect.centery = self.screen_rect.centery
+
+
+    def changex(self, x):
+        self.rect.centerx = self.rect.centerx + x
 
     def blitme(self):
         """Draw the logo at current location."""
