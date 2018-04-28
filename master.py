ef92b80ef8195cad0ffd44b386c1b08f60d6f2e0
Philip James <pjkelnhofer@gmail.com>
2018-04-28 15:36:27 -0400

    Trying to make sure Radio object is cleaned up

--- master.py
+++ master.py
@@ -4,13 +4,6 @@ from radio import Radio
 from edit import Editor
 
 while True:
-    print("Making the editor")
     editor = Editor()
-    print("Editor has closed you can do something else...")
-    editor = None
-
-
-    print("Making the radio")
     radio = Radio()
-    print("Radio closed you do something else ... ")
-    radio = None
+    del radio
